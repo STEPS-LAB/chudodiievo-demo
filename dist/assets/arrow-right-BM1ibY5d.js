11260155cd77ed75ecf8089179757a1603f8dcf3
alexsomeone1 <93034733+alexsomeone1@users.noreply.github.com>
2026-04-01 17:25:52 +0300

    change repo

--- dist/assets/arrow-right-BM1ibY5d.js
+++ dist/assets/arrow-right-BM1ibY5d.js
@@ -1,4 +1,4 @@
-import{c as r}from"./index-BNmb3mv5.js";/**
+import{c as r}from"./index-CkG3AGnd.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
